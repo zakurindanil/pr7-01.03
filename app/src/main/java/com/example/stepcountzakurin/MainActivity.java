12012b627d64dfaf77510ca6c175e6f133b33a03
danil <danil@DESKTOP-T4KQBOA>
2026-05-09 20:27:41 +0500

    программирование окна MainActivity закурин

--- app/src/main/java/com/example/stepcountzakurin/MainActivity.java
+++ app/src/main/java/com/example/stepcountzakurin/MainActivity.java
@@ -1,24 +1,61 @@
 package com.example.stepcountzakurin;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    public boolean Active = false;
+    SensorManager SensorManager;
+    int Count = 0;
+    TextView tvResult;
+    Button btnPause;
+    long LastTimeUpdate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        tvResult = findViewById(R.id.tvResult);
+        btnPause = findViewById(R.id.btnPause);
+        tvResult.setText(String.valueOf(Count));
+        SensorManager = (android.hardware.SensorManager) getSystemService(SENSOR_SERVICE);
+
+        SensorManager.registerListener((SensorEventListener) this,
+                SensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                android.hardware.SensorManager.SENSOR_DELAY_FASTEST);
+        LastTimeUpdate = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onResunme(){
+        super.onResume();
+
+        SensorManager.registerListener((SensorEventListener) this,
+                SensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                android.hardware.SensorManager.SENSOR_DELAY_FASTEST);
+        LastTimeUpdate = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        SensorManager.unregisterListener((SensorEventListener) this);
+    }
+
+    public void OnStop(View view){
+        Active = !Active;
+        if(Active) btnPause.setText("ПАУЗА");
+        else btnPause.setText("ВОЗОБНОВИТЬ");
     }
 }
